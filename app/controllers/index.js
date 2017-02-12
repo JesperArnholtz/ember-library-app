6c562638be55faaa707895896186ffe79f62fab8
Jesper Nørløv Arnholtz <jna@pentia.dk>
2017-02-12 18:22:17 +0100

    adding admin and library new links

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,28 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid'),
-
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
-  // actualEmailAddress: Ember.computed('emailAddress', function() { 
-  //   console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  // }),
-  //
-  // emailAddressChanged: Ember.observer('emailAddress', function() { 
-  //   console.log('observer is called', this.get('emailAddress')); 
-  // }),
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
 
   actions: {
 
     saveInvitation() {
       const email = this.get('emailAddress');
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
 
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
+
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+
     }
   }
+
 });
